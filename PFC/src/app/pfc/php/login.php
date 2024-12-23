bbf6d32d866b3f2329333ad21a300741ad9c5416
RodrigoMc016 <piedradecoloresxd@gmail.com>
2024-12-23 14:08:20 +0100

    feat: cambio de urls

--- PFC/src/app/pfc/php/login.php
+++ PFC/src/app/pfc/php/login.php
@@ -5,8 +5,11 @@ require 'conexion.php'; // Conexión a la base de datos
 header("Access-Control-Allow-Origin: *");
 header("Access-Control-Allow-Headers: Content-Type");
 header("Access-Control-Allow-Methods: GET, POST, OPTIONS");
-header("Content-Type: application/json; charset=UTF-8");
-
+header("Content-Type: application/json; charset=UTF-8, Authorization");
+if ($_SERVER['REQUEST_METHOD'] == 'OPTIONS') {
+  http_response_code(200);
+  exit;
+}
 
 
 $inputData = json_decode(file_get_contents('php://input'), true);
--- PFC/src/app/pfc/php/registro.php
+++ PFC/src/app/pfc/php/registro.php
@@ -8,6 +8,10 @@ header("Access-Control-Allow-Origin: *");
 header("Access-Control-Allow-Methods: POST, GET, OPTIONS");
 header('Content-Type: application/json');
 header("Access-Control-Allow-Headers: Content-Type, Authorization");
+if ($_SERVER['REQUEST_METHOD'] == 'OPTIONS') {
+  http_response_code(200);
+  exit;
+}
 
 require 'conexion.php';
 
